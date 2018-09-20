8dba72d3ce785c60b720a934d6d098b103e149b3
Erik <vfxbaliof@gmail.com>
2018-09-20 22:06:19 +0300

    01. Create Tables

---
+++ INTRODUCTION TO DATABASES. DATA DEFINITION AND DATATYPES - EXERCISE/01. Create Tables/01. Create Tables.sql
@@ -0,0 +1,10 @@
+USE minions;
+
+CREATE TABLE minions(
+id INT(11), 
+name VARCHAR(255), 
+age INT(11), 
+PRIMARY KEY (id));
+CREATE TABLE towns(id INT(11), 
+name VARCHAR(255), 
+PRIMARY KEY (id));
