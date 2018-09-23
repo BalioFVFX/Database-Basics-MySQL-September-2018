6ce288dbea0666403b9c012b426a26637446df86
Erik <vfxbaliof@gmail.com>
2018-09-23 16:05:24 +0300

    16. Basic Insert

---
+++ INTRODUCTION TO DATABASES. DATA DEFINITION AND DATATYPES - EXERCISE/16. Basic Insert/16. Basic Insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO towns(name) values ('Sofia');
+INSERT INTO towns(name) values ('Plovdiv');
+INSERT INTO towns(name) values ('Varna');
+INSERT INTO towns(name) values ('Burgas');
+
+INSERT INTO departments(name) values('Engineering');
+INSERT INTO departments(name) values('Sales');
+INSERT INTO departments(name) values('Marketing');
+INSERT INTO departments(name) values('Software Development');
+INSERT INTO departments(name) values('Quality Assurance');
+
+INSERT INTO employees(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) values
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00);
+
+INSERT INTO employees(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) values
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00);
+
+INSERT INTO employees(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) values
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25);
+
+INSERT INTO employees(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) values
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00);
+
+INSERT INTO employees(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) values
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
