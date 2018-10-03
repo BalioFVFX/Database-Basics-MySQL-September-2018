3584543b3286d14e047f8b7d5e47a05d4e49e4e6
Erik <vfxbaliof@gmail.com>
2018-10-03 11:30:28 +0300

    1. Departments Info

--- DATA AGGREGATION/1. Departments Info/1. Departments Info.sql
+++ DATA AGGREGATION/1. Departments Info/1. Departments Info.sql
@@ -1,3 +1,3 @@
-SELECT department_id, COUNT(id) as 'Number of employees' FROM employees
+SELECT department_id, COUNT(id) AS 'Number of employees' FROM employees
 GROUP BY department_id
 ORDER BY department_id, COUNT(id);
