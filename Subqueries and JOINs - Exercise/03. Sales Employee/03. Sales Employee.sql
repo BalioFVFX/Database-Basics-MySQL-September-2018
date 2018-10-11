fd5230679248849808aa24d702064f4da0b92e62
Erik <vfxbaliof@gmail.com>
2018-10-11 09:30:48 +0300

    03. Sales Employee

---
+++ Subqueries and JOINs - Exercise/03. Sales Employee/03. Sales Employee.sql
@@ -0,0 +1,10 @@
+SELECT
+e.employee_id,
+e.first_name,
+e.last_name,
+d.name AS `department_name`
+FROM employees e
+JOIN departments d
+ON d.department_id = e.department_id
+WHERE d.name = 'Sales'
+ORDER BY employee_id DESC;
