4699a4829e09a3e8652cb2acf0e4302588a0e581
Erik <vfxbaliof@gmail.com>
2018-10-11 11:14:34 +0300

    07. Employees with Project

---
+++ Subqueries and JOINs - Exercise/07. Employees with Project/07. Employees with Project.sql
@@ -0,0 +1,12 @@
+SELECT
+e.employee_id,
+e.first_name,
+p.name AS `project_name`
+FROM employees e
+JOIN employees_projects ep
+ON ep.employee_id = e.employee_id
+JOIN projects p
+ON p.project_id = ep.project_id
+WHERE DATE(p.start_date) > DATE('2002-08-13') AND p.end_date IS NULL
+ORDER BY e.first_name, p.name
+LIMIT 5;
