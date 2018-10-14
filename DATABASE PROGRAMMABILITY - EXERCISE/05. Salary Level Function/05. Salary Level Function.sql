ed1e47a8b9e34b350c126991a8eae9a359c40dae
Erik <vfxbaliof@gmail.com>
2018-10-14 12:56:30 +0300

    05. Salary Level Function

---
+++ DATABASE PROGRAMMABILITY - EXERCISE/05. Salary Level Function/05. Salary Level Function.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION ufn_get_salary_level(input_salary DOUBLE)
+RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN
+DECLARE result VARCHAR(10);
+RETURN CASE
+WHEN input_salary < 30000 THEN 'Low'
+WHEN input_salary >= 30000 AND input_salary <= 50000 THEN 'Average'
+WHEN input_salary > 50000 THEN 'High'
+END;
+END
